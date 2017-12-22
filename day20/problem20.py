44331c2e73e8a49fbf9d1f122b646af12d024162
edipascale <dipascae@tcd.ie>
2017-12-22 16:44:41 +0100

    solution for part 2, but I had to go for a heurstic stopping condition. Wonder how I can be sure that there is not going to be more collisions (without checking every possible pair of particles for position, velocity and acceleration)

--- day20/problem20.py
+++ day20/problem20.py
@@ -26,5 +26,48 @@ def part_one(filename):
             candidates = [x for x in newCandidates if pos[x] == m]
             print(candidates)
 
+def distance(a, b):
+    return sum(list(map(abs, (b[i] - a[i] for i in range(3)))))
+
+def part_two(filename):
+    pos, vel, acc = {}, {}, {}
+    remaining = set()
+    with open(filename, 'r') as f:
+        for i, line in enumerate(f):
+            remaining.add(i)
+            start = line.index('<') + 1
+            end = line.index('>')
+            pos[i] = list(map(int,line[start:end].split(',')))
+            start = line.index('<', end) + 1
+            end = line.index('>', start)
+            vel[i] = list(map(int,line[start:end].split(',')))
+            start = line.index('<', end) + 1
+            end = line.index('>', start)
+            acc[i] = list(map(int,line[start:end].split(',')))
+    lastCollision = 0
+    while lastCollision < 1000:
+        # update all positions and velocities
+        currentPos = {}
+        collided = set()
+        for x in remaining:
+            for i in range(3):
+                vel[x][i] += acc[x][i]
+                pos[x][i] += vel[x][i]
+            # check for collisions
+            if pos[x] in currentPos.values():
+                collided.add(x)
+                collided = collided | set([elem for elem in currentPos.keys()
+                                          if currentPos[elem] == pos[x]])
+            else :
+                currentPos[x] = pos[x]
+        remaining = remaining - collided
+        if len(remaining) < 2:
+            break
+        if len(collided) > 0:
+            lastCollision = -1
+        lastCollision += 1
+    print(len(remaining))
+
 
 part_one("input.txt")
+part_two("input.txt")
